3b85dde9654678b9ee5195a0a065cfc8852facd1
anshiky73-svg <anshiky73@gmail.com>
2026-01-27 12:51:25 +0530

    [feat]: Added a badge system for the users depending on the number of questions they have solved

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardLayout from "./layouts/DashboardLayout";
 import Index from "./pages/Index";
 import DSA from "./pages/DSA";
-
 import MockTest from "./pages/MockTest";
 import AptitudePage from "./pages/AptitudePage";
 import ProblemDetail from "./pages/ProblemDetail";
-
 import MyProgress from "./pages/MyProgress";
-
 import Companies from "./pages/Companies";
-
 import StudyMaterial from "./pages/StudyMaterial"; 
 import SystemDesign from "./pages/SystemDesign";
-
 import ComingSoon from "./pages/ComingSoon";
 import NotFound from "./pages/NotFound";
-
 import Settings from "./pages/Settings";
 import ScrollToTop from "./components/ScrollToTop";
-import ResumeBuilder from "./pages/ResumeBuilder";   // ✅ NEW IMPORT
-
-
+import ResumeBuilder from "./pages/ResumeBuilder";
 import Mentorship from "./pages/Mentorship";
-
+import Progress from "./pages/Progress";  // ✅ Badge system component
 
 const queryClient = new QueryClient();
 
@@ -37,34 +29,30 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route element={<DashboardLayout />}>
             <Route path="/" element={<Index />} />
             <Route path="/dsa" element={<DSA />} />
-
             <Route path="/study-material" element={<StudyMaterial />} />
             <Route path="/dsa/problem/:id" element={<ProblemDetail />} />
             <Route path="/system-design" element={<SystemDesign />} />
-
             <Route path="/companies" element={<Companies />} />
             <Route path="/my-progress" element={<MyProgress />} />
             <Route path="/mentorship" element={<Mentorship />} />
             <Route path="/mock-test" element={<MockTest />} />
-
             <Route path="/aptitude-test" element={<AptitudePage />} />
-            <Route path="/interview" element={<ComingSoon />} />
             <Route path="/settings" element={<Settings />} />
-
-
+            <Route path="/resume" element={<ResumeBuilder />} />
+            
+            {/* ✅ FIXED: Badge System Progress Page */}
+            <Route path="/progress" element={<Progress />} />
+            
+            {/* Coming Soon Pages */}
             <Route path="/aptitude" element={<ComingSoon />} />
-
             <Route path="/interview" element={<ComingSoon />} />
-            <Route path="/progress" element={<ComingSoon />} />
             <Route path="/materials" element={<ComingSoon />} />
-            <Route path="/resume" element={<ComingSoon />} />
             <Route path="/tests" element={<ComingSoon />} />
-            <Route path="/mentorship" element={<Mentorship />} />
-            <Route path="/settings" element={<ComingSoon />} />
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
